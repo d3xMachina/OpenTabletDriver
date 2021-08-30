c80aa0c9701edc31da62ed679434bca00e8f1b60
fab <email@example.com>
2021-08-30 04:32:09 +0200

    Custom publish script for windows

---
+++ PUBLISH.bat
@@ -0,0 +1,12 @@
+@echo off
+set "options=--nologo --configuration Release -p:PublishSingleFile=true --self-contained false"
+set "runtime=win10-x64"
+set "framework=net5"
+set "frameworkwindows=net5-windows"
+
+dotnet publish OpenTabletDriver.Daemon %options% --runtime %runtime% --framework %frameworkwindows% -o build/%runtime%
+dotnet publish OpenTabletDriver.Console %options% --runtime %runtime% --framework %framework% -o build/%runtime%
+dotnet publish OpenTabletDriver.UX.Wpf %options% --runtime %runtime% --framework %frameworkwindows% -o build/%runtime%
+robocopy "OpenTabletDriver/Configurations/" "build/%runtime%/Configurations/" /s /e >nul
+pause
+
